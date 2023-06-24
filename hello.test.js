199f2b3ca6d320053b717d2805e18576ee2cddba
JaimeCastaneda <jaime.castaneda@itpaero.com>
2023-06-24 14:19:48 +0200

    Spain

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola Mundo desde Madrid");
+    expect(hello.hello()).toEqual("Hola Mundo desde España");
   });
 });
